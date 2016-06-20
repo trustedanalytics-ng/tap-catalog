1bb9f1d40c703b93113b5ac449d2d733b5eea458
jwierzbo <jakub.a.wierzbowski@intel.com>
2016-06-20 14:58:45 +0200

    Etcd client

---
+++ etcd/etcd_client.go
@@ -0,0 +1,123 @@
+package etcd
+
+import (
+	"log"
+	"time"
+
+	"github.com/coreos/etcd/client"
+	"golang.org/x/net/context"
+
+	"encoding/json"
+	"github.com/trustedanalytics/tap-go-common/logger"
+)
+
+type EtcdKVStore interface {
+	GetKeyValue(key string) (string, error)
+	GetKeyIntoStruct(key string, result interface{}) error
+	Set(key string, value interface{}) error
+	Update(key string, value interface{}) error
+	Delete(key string) error
+}
+
+type EtcdConnector struct{}
+
+var logger = logger_wrapper.InitLogger("etcd")
+
+func (c *EtcdConnector) GetKeyValue(key string) (string, error) {
+	logger.Debug("Getting value of key:", key)
+	result := ""
+	err := c.GetKeyIntoStruct(key, &result)
+	return result, err
+}
+
+func (c *EtcdConnector) GetKeyIntoStruct(key string, result interface{}) error {
+	logger.Debug("Getting value of key:", key)
+	kapi, err := getKVApiV2Connector()
+	if err != nil {
+		logger.Error("Can't connect with ETCD:", err)
+		return err
+	}
+
+	resp, err := kapi.Get(context.Background(), key, nil)
+	if err != nil {
+		logger.Error("Getinng key value error:", err)
+		return err
+	}
+	err = json.Unmarshal([]byte(resp.Node.Value), result)
+	return nil
+}
+
+func (c *EtcdConnector) Set(key string, value interface{}) error {
+	logger.Debug("Getting value of key:", key)
+	valueByte, err := json.Marshal(value)
+	if err != nil {
+		logger.Error("Can't marshall etcd key value!:", err)
+		return err
+	}
+
+	kapi, err := getKVApiV2Connector()
+	if err != nil {
+		logger.Error("Can't connect with ETCD:", err)
+		return err
+	}
+
+	_, err = kapi.Set(context.Background(), key, string(valueByte), nil)
+	if err != nil {
+		log.Println("Setting key value error", err)
+		return err
+	}
+	return nil
+}
+
+func (c *EtcdConnector) Update(key string, value interface{}) error {
+	logger.Debug("Updating value of key:", key)
+	valueByte, err := json.Marshal(value)
+	if err != nil {
+		logger.Error("Can't marshall etcd key value!:", err)
+		return err
+	}
+
+	kapi, err := getKVApiV2Connector()
+	if err != nil {
+		logger.Error("Can't connect with ETCD:", err)
+		return err
+	}
+
+	_, err = kapi.Update(context.Background(), key, string(valueByte))
+	if err != nil {
+		log.Println("Updating key value error", err)
+		return err
+	}
+	return nil
+}
+
+func (c *EtcdConnector) Delete(key string) error {
+	logger.Debug("Deleting value of key:", key)
+	kapi, err := getKVApiV2Connector()
+	if err != nil {
+		logger.Error("Can't connect with ETCD:", err)
+		return err
+	}
+
+	_, err = kapi.Delete(context.Background(), key, nil)
+	if err != nil {
+		logger.Error("Getinng key value error:", err)
+		return err
+	}
+	return nil
+}
+
+func getKVApiV2Connector() (client.KeysAPI, error) {
+	cfg := client.Config{
+		Endpoints: []string{"http://127.0.0.1:2379"},
+		Transport: client.DefaultTransport,
+		// set timeout per request to fail fast when the target endpoint is unavailable
+		HeaderTimeoutPerRequest: time.Second,
+	}
+	c, err := client.New(cfg)
+	if err != nil {
+		log.Println("connection error:", err)
+		return nil, err
+	}
+	return client.NewKeysAPI(c), nil
+}
---
+++ etcd/etcd_test.go
@@ -0,0 +1,36 @@
+// +build dev_test
+package etcd
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestEtcdKVStore(t *testing.T) {
+	client := EtcdConnector{}
+	Convey("Test EtcdKVStore", t, func() {
+		Convey("Should update properly", func() {
+			err := client.Set("test", "testValue")
+			So(err, ShouldBeNil)
+		})
+
+		Convey("Should get KV properly", func() {
+			value, err := client.GetKeyValue("test")
+			So(err, ShouldBeNil)
+			So(value, ShouldEqual, "testValue")
+		})
+
+		Convey("Should get KV into struct properly", func() {
+			result := ""
+			err := client.GetKeyIntoStruct("test", &result)
+			So(err, ShouldBeNil)
+			So(result, ShouldEqual, "testValue")
+		})
+
+		Convey("Should delete KV properly", func() {
+			err := client.Delete("test")
+			So(err, ShouldBeNil)
+		})
+	})
+}
