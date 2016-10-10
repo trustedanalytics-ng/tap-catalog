d49a043b2599ddb18730bbe01c5c704058344f14
pzelazko-intel <pawel.zelazko@intel.com>
2016-10-10 08:35:25 +0200

    DPNG-10477 added InstanceDependency to Application model (#62)

--- models/application.go
+++ models/application.go
@@ -1,11 +1,22 @@
 package models
 
+import "reflect"
+
+func init() {
+	RegisterType("InstanceDependencies", reflect.TypeOf(InstanceDependency{}))
+}
+
+type InstanceDependency struct {
+	Id string `json:"id"`
+}
+
 type Application struct {
-	Id          string     `json:"id"`
-	Name        string     `json:"name"`
-	Description string     `json:"description"`
-	ImageId     string     `json:"imageId"`
-	Replication int        `json:"replication"`
-	TemplateId  string     `json:"templateId"`
-	AuditTrail  AuditTrail `json:"auditTrail"`
+	Id                   string               `json:"id"`
+	Name                 string               `json:"name"`
+	Description          string               `json:"description"`
+	ImageId              string               `json:"imageId"`
+	Replication          int                  `json:"replication"`
+	TemplateId           string               `json:"templateId"`
+	AuditTrail           AuditTrail           `json:"auditTrail"`
+	InstanceDependencies []InstanceDependency `json:"instanceDependencies"`
 }
