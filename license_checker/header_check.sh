8cd727ddca9b33e6914a193ba23c7b77a23ce08b
Marcin Makowski <marcin.makowski@intel.com>
2016-11-16 13:18:01 +0100

    add license checker, fix shell script license

--- license_checker/header_check.sh
+++ license_checker/header_check.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2016 Intel Corporation
+# Copyright (c) 2015 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-#!/bin/bash
-make build_anywhere
+mvn -f license_checker/license_checker.xml license:check
+exit $?
