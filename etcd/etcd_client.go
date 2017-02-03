f4ed21d9ed659461f51d0a53233bf0fff059bffd
pzelazko-intel <pawel.zelazko@intel.com>
2017-02-03 16:15:09 +0100

    DPNG-14957 several etcd instances supported (#129)
    DPNG-14957 several etcd instances supported

--- etcd/etcd_client.go
+++ etcd/etcd_client.go
@@ -18,6 +18,7 @@ package etcd
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/coreos/etcd/client"
@@ -45,15 +46,16 @@ type EtcdKVStore interface {
 }
 
 type EtcdConnector struct {
-	address string
-	port    int
-	keysAPI client.KeysAPI
+	addresses []string
+	keysAPI   client.KeysAPI
 }
 
 var logger, _ = commonLogger.InitLogger("etcd")
 
-func NewEtcdKVStore(address string, port int) (EtcdKVStore, error) {
-	res := &EtcdConnector{address: address, port: port}
+//takes address in form of "https://hostname:port,https://hostname2:port2"
+func NewEtcdKVStore(addresses string) (EtcdKVStore, error) {
+	splitAddresses := strings.Split(addresses, ",")
+	res := &EtcdConnector{addresses: splitAddresses}
 	err := res.Connect()
 	return res, err
 }
@@ -63,7 +65,7 @@ func (c *EtcdConnector) Connect() error {
 	headerTimeout := time.Duration(headerTimeoutFromEnv) * time.Millisecond
 
 	cfg := client.Config{
-		Endpoints:               []string{fmt.Sprintf("https://%s:%d", c.address, c.port)},
+		Endpoints:               c.addresses,
 		Transport:               client.DefaultTransport,
 		HeaderTimeoutPerRequest: headerTimeout,
 	}
